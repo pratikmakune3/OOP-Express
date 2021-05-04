922e1412a20854e279c17ff10b99e1c9f6f3bdb1
pratikmakune3 <pratikm279@gmail.com>
2021-05-04 13:48:00 +0530

    Added: Commented functional programming code

--- src/Router.js
+++ src/Router.js
@@ -1,9 +1,23 @@
-module.exports = (app) => {
-  app.get("/", (req, res, next) => res.send("Hey there!"));
+// module.exports = (app) => {
+//   app.get("/", (req, res, next) => res.send("Hey there!"));
 
-  app.get("/user/:username", (req, res, next) => {
-    const { username } = req.params;
+//   app.get("/user/:username", (req, res, next) => {
+//     const { username } = req.params;
 
-    res.send(`Hey ${username}!!!`);
-  });
-};
+//     res.send(`Hey ${username}!!!`);
+//   });
+// };
+
+class Router {
+  constructor(app) {
+    app.get("/", (req, res, next) => res.send("Hey there!"));
+
+    app.get("/user/:username", (req, res, next) => {
+      const { username } = req.params;
+
+      res.send(`Hey ${username}!!!`);
+    });
+  }
+}
+
+module.exports = Router;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,20 @@
+/* 
+  Functional Way to create express app 
+*/
+// const express = require("express");
+
+// const app = express();
+
+// app.get("/", (req, res, next) => res.send("Hey there"));
+
+// app.listen(3001, () => console.log("Listening on port 3001"));
+
+/* 
+  OOP way to create express app
+*/
 const App = require("./App");
 const getRoutes = require("./Router");
 
 const app = new App(3001);
 
-getRoutes(app);
+new getRoutes(app);
